7b81c8e0500ba0d79360116af373a0923811c776
xStephx <bojkovskistefan9@gmail.com>
2024-07-01 12:43:23 +0200

    added animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,21 @@ for (let i = 0; i < questions.length; i++) {
   questions[i].addEventListener('click', function() {
     for (let j = 0; j < answers.length; j++) {
       if (j !== i) {
-        answers[j].classList.add('hidden');
+        answers[j].classList.remove('max-h-64');
+        answers[j].classList.add('max-h-0');
         plusIcons[j].classList.remove('hidden');
         minusIcons[j].classList.add('hidden');
       }
     }
     // Toggle visibility for the clicked question's answer
-    if (answers[i].classList.contains('hidden')) {
-      answers[i].classList.remove('hidden');
+    if (answers[i].classList.contains('max-h-0')) {
+      answers[i].classList.remove('max-h-0');
+      answers[i].classList.add('max-h-64');
       plusIcons[i].classList.add('hidden');
       minusIcons[i].classList.remove('hidden');
     } else {
-      answers[i].classList.add('hidden');
+      answers[i].classList.remove('max-h-64');
+      answers[i].classList.add('max-h-0');
       plusIcons[i].classList.remove('hidden');
       minusIcons[i].classList.add('hidden');
     }
